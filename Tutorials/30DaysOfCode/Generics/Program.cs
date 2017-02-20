199fb1e3e60b04b0851b2f02dec3015b039ff47f
halityurtsever <halityurtsever@gmail.com>
2017-02-20 10:55:55 +0300

    Generics task is added in 30 days of code.

---
+++ Tutorials/30DaysOfCode/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            PrintArray<int>(intArray);
+            PrintArray<string>(stringArray);
+        }
+
+        private static void PrintArray<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
