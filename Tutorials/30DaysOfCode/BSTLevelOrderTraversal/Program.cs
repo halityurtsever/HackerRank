d122ecb72faca72c1adf2d34ceed38b6bf03700b
halityurtsever <halityurtsever@gmail.com>
2017-02-23 09:52:07 +0300

    30 days of code. BFS - Level Order Traversal task is added.

---
+++ Tutorials/30DaysOfCode/BSTLevelOrderTraversal/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Tutorials._30DaysOfCode.BSTLevelOrderTraversal
+{
+    class Program
+    {
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
+
+        }
+
+        private static Queue<Node> queue = new Queue<Node>();
+
+        private static void levelOrder(Node root)
+        {
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                var currentNode = queue.Dequeue();
+                Console.Write($"{currentNode.data} ");
+
+                if (currentNode.left != null)
+                {
+                    queue.Enqueue(currentNode.left);
+                }
+
+                if (currentNode.right != null)
+                {
+                    queue.Enqueue(currentNode.right);
+                } 
+            }
+        }
+    }
+
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
