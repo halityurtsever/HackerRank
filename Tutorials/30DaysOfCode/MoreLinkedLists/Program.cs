62cc1b1f5981cf12fbf2ab1905cd5ce09b69bada
halityurtsever <halityurtsever@gmail.com>
2017-02-23 10:10:37 +0300

    Namespace is updated.

--- Tutorials/30DaysOfCode/MoreLinkedLists/Program.cs
+++ Tutorials/30DaysOfCode/MoreLinkedLists/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MoreLinkedLists
+namespace HackerRank.Tutorials._30DaysOfCode.MoreLinkedLists
 {
     class Program
     {
