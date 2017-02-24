3f7f23e9aaf1c7f419dcd5de386ed75d391111cb
halityurtsever <halityurtsever@gmail.com>
2017-02-24 23:21:07 +0300

    30 days of code. Running time and complexity task is added.

---
+++ Tutorials/30DaysOfCode/RunningTimeAndComplexity/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HackerRank.Tutorials._30DaysOfCode.RunningTimeAndComplexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime;
+            int testCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                isPrime = true;
+                var number = Convert.ToDouble(Console.ReadLine());
+
+                if (number == 1)
+                {
+                    Console.WriteLine("Not prime");
+                    continue;
+                }
+
+                int squareRoot = Convert.ToInt32(Math.Round(Math.Sqrt(number)));
+
+                for (int j = squareRoot; j > 1; j--)
+                {
+                    if (number % j == 0)
+                    {
+                        isPrime = false;
+                        Console.WriteLine("Not prime");
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine("Prime");
+                }
+            }
+        }
+    }
+}
