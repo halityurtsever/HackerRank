1f38059d95608b67b7e5fde524d98f4abdff575f
halityurtsever <halityurtsever@gmail.com>
2017-03-01 01:04:52 +0300

    30 days of code. Bitwise AND task is added. There are test fails. Fix them.

---
+++ Tutorials/30DaysOfCode/BitwiseAND/Program.cs
@@ -0,0 +1,46 @@
+﻿using CodeHelpers;
+using System;
+
+namespace HackerRank.Tutorials._30DaysOfCode.BitwiseAND
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IConsole console = new ConsoleWrapper();
+            ExecuteTask(console);
+        }
+
+        public static void ExecuteTask(IConsole console)
+        {
+            int testCases = Convert.ToInt32(console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string[] tokens = console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(tokens[0]);
+                int k = Convert.ToInt32(tokens[1]);
+
+                int index = k;
+                int result = 0;
+                while (true)
+                {
+                    for (int j = index + 1; j <= n; j++)
+                    {
+                        if (((index & j) > result) && ((index & j) < k))
+                        {
+                            result = index & j;
+                        }
+                    }
+                    if (result > 0 || index == 1)
+                    {
+                        break;
+                    }
+                    index--;
+                }
+
+                console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Tutorials/30DaysOfCode/BitwiseAND_Tests/Program_Tests.cs
@@ -0,0 +1,59 @@
+﻿using CodeHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TestHelper;
+
+namespace HackerRank.Tutorials._30DaysOfCode.BitwiseAND.Tests
+{
+    [TestClass()]
+    public class Program_BitwiseAND_Tests : TestBase
+    {
+        //################################################################################
+        #region Tests
+
+        [TestMethod()]
+        public void ExecuteTest_1()
+        {
+            TestRunner("input_1.txt", "output_1.txt");
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Override Implementation
+
+        public override IConsole GetConsoleReader(DirectoryInfo directory, string inputFileName, string outputFileName)
+        {
+            var folderPath = directory.Parent.Parent.FullName;
+            return new ConsoleWrapperTest(folderPath, inputFileName, outputFileName);
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Private Implementation
+
+        private void TestRunner(string inputFile, string outputFile)
+        {
+            var directoryInfo = new DirectoryInfo(Environment.CurrentDirectory);
+            var console = GetConsoleReader(directoryInfo, $"{s_InputOutputFolder}/{inputFile}", $"{s_InputOutputFolder}/{outputFile}");
+
+            Program.ExecuteTask(console);
+
+            var consoleWrapper = (ConsoleWrapperTest)console;
+
+            Assert.AreEqual(consoleWrapper.ExpectedOutput.Count, consoleWrapper.ActualOutput.Count);
+
+            for (int i = 0; i < consoleWrapper.ActualOutput.Count; i++)
+            {
+                string expectedMaxValue = ((IConsoleTest)console).ReadLineFromExpectedOutput();
+                string actualMaxValue = ((IConsoleTest)console).ReadLineFromActualOutput();
+
+                Assert.AreEqual(expectedMaxValue, actualMaxValue);
+            }
+        }
+
+        #endregion
+    }
+}
