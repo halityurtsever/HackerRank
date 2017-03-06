4bc71269bcad0f465a69a920ba18743f3bc6b806
halityurtsever <halityurtsever@gmail.com>
2017-03-06 09:27:16 +0300

    Testcases are renamed.

--- _Tests/KingdomConnectivity_Tests/Program_KingdomConnectivity_Tests.cs
+++ _Tests/KingdomConnectivity_Tests/Program_KingdomConnectivity_Tests.cs
@@ -13,67 +13,67 @@ namespace HackerRank.Tracks.Algorithms.GraphTheory.KingdomConnectivity.Tests
         #region Tests
 
         [TestMethod()]
-        public void ExecuteTest_1()
+        public void KingdomConnectivity_TestCase_01()
         {
             TestRunner("input_1.txt", "output_1.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_2()
+        public void KingdomConnectivity_TestCase_02()
         {
             TestRunner("input_2.txt", "output_2.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_3()
+        public void KingdomConnectivity_TestCase_03()
         {
             TestRunner("input_3.txt", "output_3.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_4()
+        public void KingdomConnectivity_TestCase_04()
         {
             TestRunner("input_4.txt", "output_4.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_5()
+        public void KingdomConnectivity_TestCase_05()
         {
             TestRunner("input_5.txt", "output_5.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_6()
+        public void KingdomConnectivity_TestCase_06()
         {
             TestRunner("input_6.txt", "output_6.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_7()
+        public void KingdomConnectivity_TestCase_07()
         {
             TestRunner("input_7.txt", "output_7.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_8()
+        public void KingdomConnectivity_TestCase_08()
         {
             TestRunner("input_8.txt", "output_8.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_9()
+        public void KingdomConnectivity_TestCase_09()
         {
             TestRunner("input_9.txt", "output_9.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_10()
+        public void KingdomConnectivity_TestCase_10()
         {
             TestRunner("input_10.txt", "output_10.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_11()
+        public void KingdomConnectivity_TestCase_11()
         {
             TestRunner("input_11.txt", "output_11.txt");
         }
