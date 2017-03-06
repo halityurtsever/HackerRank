916c51bbcc6cfbbab827db49315a5548a4816eb4
halityurtsever <halityurtsever@gmail.com>
2017-03-06 09:25:13 +0300

    Testcases are renamed.

--- _Tests/Equal_Tests/Program_Equal_Tests.cs
+++ _Tests/Equal_Tests/Program_Equal_Tests.cs
@@ -13,67 +13,67 @@ namespace HackerRank.Tracks.Algorithms.DynamicProgramming.Equal.Tests
         #region Tests
 
         [TestMethod()]
-        public void ExecuteTest_01()
+        public void Equal_TestCase_01()
         {
             TestRunner("input_1.txt", "output_1.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_02()
+        public void Equal_TestCase_02()
         {
             TestRunner("input_2.txt", "output_2.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_03()
+        public void Equal_TestCase_03()
         {
             TestRunner("input_3.txt", "output_3.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_04()
+        public void Equal_TestCase_04()
         {
             TestRunner("input_4.txt", "output_4.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_05()
+        public void Equal_TestCase_05()
         {
             TestRunner("input_5.txt", "output_5.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_06()
+        public void Equal_TestCase_06()
         {
             TestRunner("input_6.txt", "output_6.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_07()
+        public void Equal_TestCase_07()
         {
             TestRunner("input_7.txt", "output_7.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_08()
+        public void Equal_TestCase_08()
         {
             TestRunner("input_8.txt", "output_8.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_09()
+        public void Equal_TestCase_09()
         {
             TestRunner("input_9.txt", "output_9.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_10()
+        public void Equal_TestCase_10()
         {
             TestRunner("input_10.txt", "output_10.txt");
         }
 
         [TestMethod()]
-        public void ExecuteTest_11()
+        public void Equal_TestCase_11()
         {
             TestRunner("input_11.txt", "output_11.txt");
         }
