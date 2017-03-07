7bc47a2fec6edad8f898306cedd4df70a695929a
halityurtsever <halityurtsever@gmail.com>
2017-03-07 16:55:57 +0300

    Coin array is sorted and two new method is defined with empty body.

--- Tracks/Algorithms/DynamicProgramming/TheCoinChangeProblem/Program_TheCoinChangeProblem.cs
+++ Tracks/Algorithms/DynamicProgramming/TheCoinChangeProblem/Program_TheCoinChangeProblem.cs
@@ -1,6 +1,5 @@
 ﻿using CodeHelpers;
 using System;
-using System.Collections.Generic;
 
 namespace HackerRank.Tracks.Algorithms.DynamicProgramming.TheCoinChangeProblem
 {
@@ -9,7 +8,6 @@ namespace HackerRank.Tracks.Algorithms.DynamicProgramming.TheCoinChangeProblem
         //################################################################################
         #region Fields
 
-        private static HashSet<string> m_DistinctCoinChanges = new HashSet<string>();
 
         #endregion
 
@@ -26,9 +24,14 @@ namespace HackerRank.Tracks.Algorithms.DynamicProgramming.TheCoinChangeProblem
         {
             //read values from console
             var initialValues = console.ReadLine().Split(' ');
-            int coinChangeValue = Convert.ToInt32(initialValues[0]);
+            int targetChangeValue = Convert.ToInt32(initialValues[0]);
             int coinArraySize = Convert.ToInt32(initialValues[1]);
             int[] coinArray = Array.ConvertAll(console.ReadLine().Split(' '), Int32.Parse);
+
+            SortArray(coinArray, 0, coinArray.Length - 1);
+
+            BuildCoinTree(coinArray, targetChangeValue);
+            CalculateDistinctChanges(targetChangeValue);
         }
 
         #endregion
@@ -36,7 +39,49 @@ namespace HackerRank.Tracks.Algorithms.DynamicProgramming.TheCoinChangeProblem
         //################################################################################
         #region Private Implementation
 
+        private static void BuildCoinTree(int[] array, int targetValue)
+        {
 
+        }
+
+        private static void CalculateDistinctChanges(int targetValue)
+        {
+            
+        }
+
+        private static void SortArray(int[] array, int low, int hi)
+        {
+            //Quick Sort
+            if (low < hi)
+            {
+                int p = Partition(array, low, hi);
+                SortArray(array, low, p - 1);
+                SortArray(array, p + 1, hi);
+            }
+        }
+
+        private static int Partition(int[] array, int low, int hi)
+        {
+            int pivot = array[hi];
+            int i = low - 1;
+            int temp;
+
+            for (int j = low; j <= hi - 1; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i = i + 1;
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            temp = array[i + 1];
+            array[i + 1] = array[hi];
+            array[hi] = temp;
+
+            return i + 1;
+        }
 
         #endregion
     }
