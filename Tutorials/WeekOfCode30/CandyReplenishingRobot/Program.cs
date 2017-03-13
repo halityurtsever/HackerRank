5ce748db7cef80a8123da06adb701e895620c728
halityurtsever <halityurtsever@gmail.com>
2017-03-13 12:07:09 +0300

    New track is added. Week of code 30. A new task is added to Week of Code 30. Candy replenishing robot.

---
+++ Tutorials/WeekOfCode30/CandyReplenishingRobot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Tutorials.WeekOfCode30.CandyReplenishingRobot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //read initial values
+            var initalValues = Console.ReadLine().Split(' ');
+            int candyValue = Convert.ToInt32(initalValues[0]);
+            int partyTime = Convert.ToInt32(initalValues[1]);
+            int[] candyPicks = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+
+            int totalRobotAdded = 0;
+            int currentCandies = candyValue;
+
+            for (int i = 0; i < partyTime - 1; i++)
+            {
+                currentCandies -= candyPicks[i];
+                if (currentCandies < 5)
+                {
+                    totalRobotAdded += candyValue - currentCandies;
+                    currentCandies = candyValue;
+                }
+            }
+
+            Console.WriteLine(totalRobotAdded);
+        }
+    }
+}
