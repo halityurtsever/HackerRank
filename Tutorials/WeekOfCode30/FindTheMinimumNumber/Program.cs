b723f20b43abd9e117faa59dcd233775eb95dbf4
halityurtsever <halityurtsever@gmail.com>
2017-03-14 11:22:25 +0300

    New task is added to Week of Code 30. Find the minimum number.

---
+++ Tutorials/WeekOfCode30/FindTheMinimumNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheMinimumNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int value = Convert.ToInt32(Console.ReadLine());
+            string templateLeft = string.Empty;
+            string templateMiddle = "int";
+            string templateRight = string.Empty;
+
+            for (int i = 1; i < value; i++)
+            {
+                templateLeft += "min(int, ";
+                templateRight += ")";
+            }
+
+            Console.WriteLine($"{templateLeft}{templateMiddle}{templateRight}");
+        }
+    }
+}
