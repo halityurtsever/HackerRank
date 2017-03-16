ef96ab3992c2c6182c53acfd12160ca23d5e00f2
halityurtsever <halityurtsever@gmail.com>
2017-03-16 14:45:43 +0300

    Poles task is added but not completed.

---
+++ Compete/WeekOfCode30/Poles/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace HackerRank.Competes.WeekOfCode30.Poles
+{
+    public class Program
+    {
+        //################################################################################
+        #region Fields
+
+        private static List<long> m_PossibleCosts = new List<long>();
+
+        #endregion
+
+        //################################################################################
+        #region Public Implementation
+
+        public static void Main(string[] args)
+        {
+            var initials = Console.ReadLine().Split(' ');
+            var poleCount = Convert.ToInt32(initials[0]);
+            var stackCount = Convert.ToInt32(initials[1]);
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Private Implementation
+        #endregion
+
+    }
+}
