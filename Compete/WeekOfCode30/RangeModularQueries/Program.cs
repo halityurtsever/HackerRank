8b4ac8e6cf05468e46a1821bf53c403fb142492f
halityurtsever <halityurtsever@gmail.com>
2017-03-18 06:58:22 +0300

    Range Modular Queries task is added and completed.

---
+++ Compete/WeekOfCode30/RangeModularQueries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HackerRank.Competes.WeekOfCode30.RangeModularQueries
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var initials = Console.ReadLine().Split(' ');
+            int arraySize = Convert.ToInt32(initials[0]);
+            int querySize = Convert.ToInt32(initials[1]);
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            for (int i = 0; i < querySize; i++)
+            {
+                var queryData = Console.ReadLine().Split(' ');
+                int left = Convert.ToInt32(queryData[0]);
+                int right = Convert.ToInt32(queryData[0]);
+                int mod = Convert.ToInt32(queryData[0]);
+                int remain = Convert.ToInt32(queryData[0]);
+                int count = 0;
+
+                for (int j = left; j <= right; j++)
+                {
+                    if (array[j] % mod == remain)
+                    {
+                        count++;
+                    }
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
