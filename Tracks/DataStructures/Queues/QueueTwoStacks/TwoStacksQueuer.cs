36b710f227304c16dfc2555146a61df64dc859bd
halityurtsever <halityurtsever@gmail.com>
2020-07-18 01:01:40 +0200

    Adapt QueueTwoStacks

---
+++ Tracks/DataStructures/Queues/QueueTwoStacks/TwoStacksQueuer.cs
@@ -0,0 +1,54 @@
+﻿using CodeHelpers;
+
+using System.Collections.Generic;
+
+namespace QueueTwoStacks.Library
+{
+    public class TwoStacksQueuer : ProblemSolverBase, IProblemSolver
+    {
+        //################################################################################
+        #region IProblemSolver Members
+
+        public void Solve(IConsole console)
+        {
+            Console = console;
+            var inputCount = int.Parse(Console.ReadLine());
+
+            QueueStacks(inputCount);
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Private Members
+
+        private void QueueStacks(int inputCount)
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                var inputs = Console.ReadLine().Split(' ');
+
+                if (inputs[0] == "1")
+                {
+                    list.Add(inputs[1]);
+                    continue;
+                }
+
+                if (inputs[0] == "2")
+                {
+                    list.RemoveAt(0);
+                    continue;
+                }
+
+                if (inputs[0] == "3")
+                {
+                    Console.WriteLine(list[0]);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ _Tests/QueueTwoStacks_Tests/QueueTwoStacks_Tests.cs
@@ -0,0 +1,23 @@
+﻿using CodeHelpers;
+
+using NUnit.Framework;
+
+using QueueTwoStacks.Library;
+
+namespace QueueTwoStacks_Tests
+{
+    [TestFixture]
+    public class QueueTwoStacks_Tests : TestBase
+    {
+        //################################################################################
+        #region Tests
+
+        [TestCase]
+        public void TestCase_01()
+        {
+            TestRunner<TwoStacksQueuer>("input_01.txt", "output_01.txt");
+        }
+
+        #endregion
+    }
+}
