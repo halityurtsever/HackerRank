3c6661e9dd8220da07ec24f52c8440532a8651fd
halityurtsever <halityurtsever@gmail.com>
2020-07-23 18:50:25 +0200

    xml summary is added

--- _Helpers/CodeHelpers/IConsole.cs
+++ _Helpers/CodeHelpers/IConsole.cs
@@ -39,8 +39,18 @@
 
         #endregion
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expectedValue"></param>
+        /// <returns></returns>
         bool ReadLineFromExpectedOutput(out string expectedValue);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="actualValue"></param>
+        /// <returns></returns>
         bool ReadLineFromActualOutput(out string actualValue);
     }
 }
